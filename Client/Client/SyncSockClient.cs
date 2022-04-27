f5e940650f43017f1451817cfb68a4cbeed73717
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2022-04-27 13:58:03 -0500

    Copied and pasted my test client code to help finish the client

--- Client/Client/SyncSockClient.cs
+++ Client/Client/SyncSockClient.cs
@@ -10,12 +10,91 @@ namespace Client
 
         public static void StartClient(string GameInput)
         {
+            //Data buffer for income data
+            byte[] bytes = new byte[1024];
+            //string ipAddr = "127.0.0.1";
 
+            //Try to connect to a remote device
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                //Create a TCP/IP socket
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                //Connect the socket to the remote endpoint
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket Connected to {0}", sender.RemoteEndPoint.ToString());
+
+                    //Encode the data string into a byte array
+                    byte[] msg = Encoding.ASCII.GetBytes(GameInput + "<EOF>");
+
+                    //Send the data through the socket
+                    int bytesSent = sender.Send(msg);
+
+                    //Receive The response from the remote device
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    //Release the Socket
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane);
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected Exception : {0}", ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            
+            string input = "";
+            string gameInput = "Scissors";
+            bool continuePlaying = false;
+
+
+            Console.WriteLine("Type in the port you want use to connect...");
+            input = Console.ReadLine();
+            port = Convert.ToInt32(input);
+
+            do
+            {
+                StartClient(gameInput);
+                Console.WriteLine("Would you like to play again?");
+                input = Console.ReadLine();
+
+                if (input == "YES" || input == "yes" || input == "Yes")
+                {
+                    continuePlaying = true;
+                }
+                else
+                {
+                    continuePlaying = false;
+                }
+
+
+            } while (continuePlaying == true);
+
+            Console.ReadKey();
+            return 0;
+
         }
     }
 }
