ae4ab1766f2c741b81c871957932a91d8e218369
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2022-04-26 19:38:00 -0500

    Made Client VS project
    Made the client VS project for other members to finish work on the client in its final iteration. This includes some skeleton code to get started with making the client.

---
+++ Client/Client/SyncSockClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class SyncSockClient
+    {
+        static int port;
+
+        public static void StartClient(string GameInput)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
