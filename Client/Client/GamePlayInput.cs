8c05b2c8b2a6c6406d0f621b466b0a58173b38e3
Troec <78937713+Troec@users.noreply.github.com>
2022-04-27 15:15:15 -0500

    Choice client code
    This code takes in the client's choices

--- Client/Client/GamePlayInput.cs
+++ Client/Client/GamePlayInput.cs
@@ -5,6 +5,26 @@ namespace Client
 {
     internal class GamePlayInput
     {
+        public string Choice()
+        {
+            int choice;
+            String choiceString;
+            string input = "";
 
+            Console.WriteLine("Enter the number of your choice:\n1. Rock\n2. Paper\n3. Scisors\n");
+            input = Console.ReadLine();
+
+            choice = Convert.ToInt32(input);
+
+            if (choice == 1)
+                return "Rock<EOF>";
+            else if (choice == 2)
+                return "Paper<EOF>";
+            else if (Choice() == 3)
+                return "Scissors<EOF>";
+            else
+                return "Invalid Choice";
+        }
+        
     }
 }
